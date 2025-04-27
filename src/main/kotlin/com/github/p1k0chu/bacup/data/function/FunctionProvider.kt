fa1388fd6ba0b96987151f58f980c959252e512b
p1k0chu <anatoliyid0207@gmail.com>
2025-04-27 12:26:24 +0300

    add function data generator

---
+++ src/main/kotlin/com/github/p1k0chu/bacup/data/function/FunctionProvider.kt
@@ -0,0 +1,68 @@
+package com.github.p1k0chu.bacup.data.function
+
+import com.google.common.hash.Hashing
+import com.google.common.hash.HashingOutputStream
+import kotlinx.io.IOException
+import net.minecraft.data.DataOutput
+import net.minecraft.data.DataProvider
+import net.minecraft.data.DataWriter
+import net.minecraft.registry.RegistryWrapper
+import net.minecraft.util.Identifier
+import net.minecraft.util.Util
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.io.ByteArrayOutputStream
+import java.nio.file.Path
+import java.util.concurrent.CompletableFuture
+import java.util.function.Consumer
+
+abstract class FunctionProvider(
+    output: DataOutput, private val registriesFuture: CompletableFuture<RegistryWrapper.WrapperLookup>
+) : DataProvider {
+    private val pathResolver = output.getResolver(DataOutput.OutputType.DATA_PACK, "function")
+
+    abstract fun generateFunctions(wrapperLookup: RegistryWrapper.WrapperLookup, consumer: Consumer<MCFunction>)
+
+    override fun run(writer: DataWriter): CompletableFuture<*> {
+        return registriesFuture.thenCompose { wrapperLookup: RegistryWrapper.WrapperLookup ->
+            val ids = mutableSetOf<Identifier>()
+            val futures = mutableListOf<CompletableFuture<*>>()
+
+            generateFunctions(wrapperLookup) { func ->
+                if (!ids.add(func.id)) {
+                    throw IllegalStateException("Duplicate function ${func.id}")
+                }
+                val path = pathResolver.resolve(func.id, "mcfunction")
+                futures.add(writeToFile(writer, func.body, path))
+            }
+
+            CompletableFuture.allOf(*futures.toTypedArray())
+        }
+    }
+
+    override fun getName(): String = "Functions"
+
+    companion object {
+        private val LOGGER: Logger = LoggerFactory.getLogger(FunctionProvider::class.java)
+
+        private fun writeToFile(
+            writer: DataWriter,
+            funcBody: String,
+            path: Path?
+        ) = CompletableFuture.runAsync({
+            try {
+                val byteArrayOutputStream = ByteArrayOutputStream()
+                val hashingOutputStream = HashingOutputStream(Hashing.sha1(), byteArrayOutputStream)
+
+                hashingOutputStream.writer().use { it.write(funcBody) }
+                writer.write(path, byteArrayOutputStream.toByteArray(), hashingOutputStream.hash())
+            } catch (e: IOException) {
+                LOGGER.error("Failed to save file to {}", path, e)
+            }
+        }, Util.getMainWorkerExecutor().named("saveStable"))
+    }
+}
+
+data class MCFunction(
+    val id: Identifier, val body: String
+)
