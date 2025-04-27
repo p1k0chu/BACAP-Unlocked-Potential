bdf0cd7a2462bca62c7ccb873f25cce2c1d2121c
p1k0chu <anatoliyid0207@gmail.com>
2025-04-27 19:34:46 +0300

    fix all-cat-gifts type in chat

--- src/main/kotlin/com/github/p1k0chu/bacup/function/reward/generator/AdventureTabRewardsGenerator.kt
+++ src/main/kotlin/com/github/p1k0chu/bacup/function/reward/generator/AdventureTabRewardsGenerator.kt
@@ -3,6 +3,7 @@ package com.github.p1k0chu.bacup.function.reward.generator
 import com.github.p1k0chu.bacup.advancement.generator.AdventureTabGenerator
 import com.github.p1k0chu.bacup.function.FunctionGenerator
 import com.github.p1k0chu.bacup.function.MCFunction
+import com.github.p1k0chu.bacup.function.reward.AdvancementType
 import com.github.p1k0chu.bacup.function.reward.rewardsBuilder
 import net.minecraft.registry.RegistryWrapper
 import java.util.function.Consumer
@@ -12,7 +13,9 @@ object AdventureTabRewardsGenerator : FunctionGenerator {
         rewardsBuilder(consumer) {
             tab(AdventureTabGenerator.TAB_NAME) {
                 advancement(AdventureTabGenerator.CAT_GIFT)
-                advancement(AdventureTabGenerator.ALL_CAT_GIFTS)
+                advancement(AdventureTabGenerator.ALL_CAT_GIFTS) {
+                    type = AdvancementType.GOAL
+                }
             }
         }
     }
