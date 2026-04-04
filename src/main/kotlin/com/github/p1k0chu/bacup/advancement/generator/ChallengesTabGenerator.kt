a67ccbc84a2a219bcafdf215baf6429fefce80f2
p1k0chu <p1k0chu.vim@gmail.com>
2026-04-04 21:24:38 +0300

    move mob/item lists to their own Costants classes
    all in one place, so its easier to manage

--- src/main/kotlin/com/github/p1k0chu/bacup/advancement/generator/ChallengesTabGenerator.kt
+++ src/main/kotlin/com/github/p1k0chu/bacup/advancement/generator/ChallengesTabGenerator.kt
@@ -6,11 +6,11 @@ import com.github.p1k0chu.bacup.advancement.AdvancementType
 import com.github.p1k0chu.bacup.advancement.advancement
 import com.github.p1k0chu.bacup.advancement.criteria.Criteria
 import com.github.p1k0chu.bacup.advancement.criteria.SingleEntityCriterion
+import com.github.p1k0chu.bacup.constants.MobConstants
 import net.minecraft.advancements.criterion.EntityPredicate
 import net.minecraft.core.HolderLookup
 import net.minecraft.core.registries.Registries
 import net.minecraft.data.advancements.AdvancementSubProvider.createPlaceholder
-import net.minecraft.world.entity.EntityType
 import net.minecraft.world.item.Items
 import java.util.*
 
@@ -29,7 +29,7 @@ object ChallengesTabGenerator : AdvancementGenerator {
             }
             exp = 200
 
-            KILLABLE_MOBS.forEach { mob ->
+            MobConstants.ANVIL_KILLABLE_MOBS.forEach { mob ->
                 addCriterion(mob.builtInRegistryHolder().registeredName, Criteria.ANVIL_KILL.createCriterion(
                     SingleEntityCriterion.Conditions(
                         Optional.empty(),
@@ -43,83 +43,4 @@ object ChallengesTabGenerator : AdvancementGenerator {
             }
         }
     }
-
-    private val KILLABLE_MOBS = listOf(
-        EntityType.ALLAY,
-        EntityType.ARMADILLO,
-        EntityType.AXOLOTL,
-        EntityType.BAT,
-        EntityType.CAMEL,
-        EntityType.CAT,
-        EntityType.CHICKEN,
-        EntityType.COD,
-        EntityType.COW,
-        EntityType.DONKEY,
-        EntityType.FROG,
-        EntityType.GLOW_SQUID,
-        EntityType.HORSE,
-        EntityType.MOOSHROOM,
-        EntityType.MULE,
-        EntityType.OCELOT,
-        EntityType.PARROT,
-        EntityType.PIG,
-        EntityType.PUFFERFISH,
-        EntityType.RABBIT,
-        EntityType.SALMON,
-        EntityType.SHEEP,
-        EntityType.SKELETON_HORSE,
-        EntityType.SNIFFER,
-        EntityType.SNOW_GOLEM,
-        EntityType.SQUID,
-        EntityType.STRIDER,
-        EntityType.TADPOLE,
-        EntityType.TROPICAL_FISH,
-        EntityType.TURTLE,
-        EntityType.VILLAGER,
-        EntityType.WANDERING_TRADER,
-        EntityType.BEE,
-        EntityType.CAVE_SPIDER,
-        EntityType.DOLPHIN,
-        EntityType.DROWNED,
-        EntityType.ENDERMAN,
-        EntityType.FOX,
-        EntityType.GOAT,
-        EntityType.IRON_GOLEM,
-        EntityType.LLAMA,
-        EntityType.PANDA,
-        EntityType.PIGLIN,
-        EntityType.POLAR_BEAR,
-        EntityType.SPIDER,
-        EntityType.TRADER_LLAMA,
-        EntityType.WOLF,
-        EntityType.ZOMBIFIED_PIGLIN,
-        EntityType.BLAZE,
-        EntityType.BOGGED,
-        EntityType.BREEZE,
-        EntityType.CREEPER,
-        EntityType.ELDER_GUARDIAN,
-        EntityType.ENDERMITE,
-        EntityType.EVOKER,
-        EntityType.GHAST,
-        EntityType.GUARDIAN,
-        EntityType.HOGLIN,
-        EntityType.HUSK,
-        EntityType.MAGMA_CUBE,
-        EntityType.PHANTOM,
-        EntityType.PIGLIN_BRUTE,
-        EntityType.PILLAGER,
-        EntityType.RAVAGER,
-        EntityType.SILVERFISH,
-        EntityType.SKELETON,
-        EntityType.SLIME,
-        EntityType.STRAY,
-        EntityType.VEX,
-        EntityType.VINDICATOR,
-        EntityType.WARDEN,
-        EntityType.WITCH,
-        EntityType.WITHER_SKELETON,
-        EntityType.ZOGLIN,
-        EntityType.ZOMBIE,
-        EntityType.ZOMBIE_VILLAGER,
-    )
 }
