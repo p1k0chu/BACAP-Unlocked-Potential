d14e7f3d65abd4a6251a0a18ae213fb8d5258b40
p1k0chu <p1k0chu.vim@gmail.com>
2026-04-05 17:09:19 +0300

    When pigs fly: require death instead of dmg
    * move LivingEntityMixin's die injection to correct place
    * clean up LivingEntityMixin and PigMixin

--- src/main/java/com/github/p1k0chu/bacup/mixin/PigMixin.java
+++ src/main/java/com/github/p1k0chu/bacup/mixin/PigMixin.java
@@ -1,23 +1,27 @@
 package com.github.p1k0chu.bacup.mixin;
 
 import com.github.p1k0chu.bacup.advancement.criteria.Criteria;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageTypes;
-import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.animal.pig.Pig;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Pig.class)
-public abstract class PigMixin extends LivingEntityMixin {
-    @Override
-    void damage(ServerLevel world, DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-        if(source.is(DamageTypes.FALL)) {
-            Entity passenger = ((Pig)(Object)this).getFirstPassenger();
+abstract class PigMixin extends LivingEntityMixin {
+    private PigMixin(EntityType<?> entityType, Level level) {
+        super(entityType, level);
+    }
 
-            if(passenger instanceof ServerPlayer player) {
+    @Override
+    protected void onDeath(DamageSource damageSource, CallbackInfo ci) {
+        super.onDeath(damageSource, ci);
+
+        if(damageSource.is(DamageTypes.FALL)) {
+            if(getFirstPassenger() instanceof ServerPlayer player) {
                 Criteria.WHEN_PIGS_FLY.trigger(player);
             }
         }
